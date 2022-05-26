d4aa1bf957e3fb5ebb8f2393f73ce22740799942
Jose <joseantonioestudiante2001@gmail.com>
2022-05-26 01:33:50 -0600

    Creado el file principal de MayCEy
    Se crea el file principal. donde se manejaran las peticiones del usuario

---
+++ MayCEy.pl
@@ -0,0 +1,17 @@
+saludos(['hola','HOLA','Hola','buenos dias','Buenos Dias','Buenos dias','buenas','buenas tardes','Buenas tardes','Buenas Tardes','buenas noches','Buenas noches','Buenas','saludos']).
+
+despedidas(['Adios','adios','Hasta luego','hasta luego','Chao','Nos vemos','nos vemos','Gracias, adios']).
+
+despedidaCambioYFuera(['Cambio y fuera','cambio y fuera']).
+
+emergencias(['Mayday','mayday','7500']).
+
+avionesPequenos(['Cessna','Beechcraft','Embraer Phenom']).
+avionesMedianos(['Boing 717','Embraer 190','Airbus A220']).
+avionesGrandes(['Boing 747','Airbus A340','Airbus A380']).
+
+solicitudEmergencias([['Perdida de motor', 'Los bomberos iran en camino'],
+      ['Parto en Medio Vuelo', 'El medico va en camino'],
+      ['Paro Cardiaco de Pasajero', 'El medico de primeros auxilios va en camino'],
+      ['Secuestro', 'Fuerza publica y el OIJ acudiran a la pista apenas lo mas rapid      o posible tras aterrizar'],
+      ['7500','Se enviaran los bomberos y la fuerza publica a la pista']]).
