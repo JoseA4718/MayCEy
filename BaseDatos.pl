48e088414f8ae6548a81b6f008554e7badca5dcd
Jose <joseantonioestudiante2001@gmail.com>
2022-05-26 22:09:23 -0600

    Mejorada la base de datos

--- BaseDatos.pl
+++ BaseDatos.pl
@@ -1,53 +1,164 @@
-saludos(['hola','HOLA','Hola','buenos dias','Buenos Dias','Buenos dias','buenas','buenas tardes','Buenas tardes','Buenas Tardes','buenas noches','Buenas noches','Buenas ','saludos']).
+:- dynamic vuelosP1/2.
+:- dynamic vuelosP21/2.
+:- dynamic vuelosP22/2.
+:- dynamic vuelosP3/2.
 
-despedidas(['Adios','Hasta luego','Cambio y fuera','Chao','Nos vemos'],'Hasta luego').
-despedidas(['adios','hasta luego','cambio y fuera','chao','nos vemos'],'hasta luego').
+saludos('hola').
+saludos('buenas').
+saludos('Hola').
+saludos('Buenas').
+saludos('Buenas tardes').
+saludos('Buenos dias').
+saludos('Buenos noches').
 
-agradecimientos(['gracias','Gracias','muchas gracias','Muchas gracias','Muchas Gracias','muy amable','Muy amable','perfecto','Perfecto'],'Con mucho gusto').
+despedidas('adios').
+despedidas('Adios').
+despedidas('hasta luego').
+despedidas('Hasta luego').
+despedidas('chao').
+despedidas('Chao').
+despedidas('hasta').
+despedidas('cambio').
 
-solicitudes(['aterrizar','despegar'],'Identifiquese, por favor').
+agradecimientos('gracias').
+agradecimientos('muchas').
 
-emergencias(['Mayday','mayday','mayday, mayday','Mayday, Mayday','MAYDAY','7500']).
+emergencia('perdida').
+emergencia('parto').
+emergencia('paro').
+emergencia('secuestro').
 
-avionesPequenos(['Cessna','Beechcraft','Embraer Phenom']).
-avionesMedianos(['Boing 717','Embraer 190','Airbus A220']).
-avionesGrandes(['Boing 747','Airbus A340','Airbus A380']).
+avionesGrandes('boing747').
+avionesGrandes('airbusa380').
+avionesGrandes('airbusa340').
+avionesMedianos('airbusa220').
+avionesMedianos('boing717').
+avionesMedianos('embraer190').
+avionesPequenos('cessna').
+avionesPequenos('beechcraft').
+avionesPequenos('embraerphenom').
 
-pista('P1','1','').
-pista('P2-1','2','Este a Oeste').
-pista('P2-2','2','Oeste a Este').
-pista('P3','3','').
+avionesDobles('boing').
+avionesDobles('airbus').
+avionesDobles('embraer').
 
-emergencias(['Perdida de motor','Llamar a bomberos'],['Parto en medio vuelo','Llamar a medico'],['Paro cardiaco de pasajero','Llamar a medico'],['Secuestro','Llamar al OIJ y fuerza publica']).
+pista('P1').
+pista('P2-1').
+pista('P2-2').
+pista('P3').cierre('cerrar').
 
-miembro(X,[X|_]):-!.
-miembro(X,[_|R]):-miembro(X,R).
+emergencias('mayday').
+emergencias('Mayday').
+emergencias('mayday,').
+emergencias('Mayday,').
+emergencias('emergencia').
+emergencias('Emergencia').
+emergencias('7500').
 
-verificarVelocidad(Vel):-
-    Vel >= 280,
-    write('Por favor disminuya su velocidad a menos de 200km/h para lograr aterrizar a tiempo'), !;
-    write('Puede aterrizar sin problemas').
+condicion('velocidad').
+condicion('peso').
+condicion('viento').
+condicion('distancia').
 
-asignarPista(Vel):-
-    verificarVelocidad(Vel).
+matricula('alpha').
+matricula('bravo').
+matricula('charlie').
+matricula('delta').
+matricula('echo').
+matricula('foxtrot').
+matricula('golf').
+matricula('hotel').
+matricula('india').
+matricula('juliet').
+matricula('kilo').
+matricula('lima').
+matricula('mike').
+matricula('november').
+matricula('oscar').
+matricula('papa').
+matricula('quebec').
+matricula('romeo').
+matricula('sierra').
+matricula('tango').
+matricula('uniform').
+matricula('victor').
+matricula('whiskey').
+matricula('xray').
+matricula('yankee').
+matricula('zulu').
 
-asignarPista(Avion, Direc):-
-    (avionesPequenos(X); avionesMedianos(X); avionesGrandes(X)),
-    miembro(Avion, X),
-    Direc == '',
-    write('Pista 3 asignada'), !.
+hora('0:00').
+hora('1:00').
+hora('2:00').
+hora('3:00').
+hora('4:00').
+hora('5:00').
+hora('6:00').
+hora('7:00').
+hora('8:00').
+hora('9:00').
+hora('10:00').
+hora('11:00').
+hora('12:00').
+hora('13:00').
+hora('14:00').
+hora('15:00').
+hora('16:00').
+hora('17:00').
+hora('18:00').
+hora('19:00').
+hora('20:00').
+hora('21:00').
+hora('22:00').
+hora('23:00').
 
-asignarPista(Avion, Direc):-
-    (avionesPequenos(X); avionesMedianos(X)),
-    miembro(Avion, X),
-    Direc == 'Este a Oeste',
-    write('Pista 2-1 asignada'), !,
-    Direc == 'Oeste a Este',
-    write('Pista 2-2 asignada'), !.
+horasPosibles(['0:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00','9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00']).
 
-asignarPista(Avion, Direc):-
-    avionesPequenos(X),
-    miembro(Avion, X),
-    Direc == '',
-    write('Pista 1 asignada'), !.
+aterrizar('aterrizar').
+despegar('despegar').
+lista('hola').
+cierre('cerrar').
 
+respuestaSaludo():-
+    nl,
+    write('Hola! En que te puedo ayudar?').
+
+respuestaIdentificacion():-
+    nl,
+    write('Identifiquese porfavor, con la matricula y avion').
+
+respuestaMatricula():-
+    nl,
+    write('Por favor, escriba su matricula').
+
+respuestaAvion():-
+    nl,
+    write('Indique el tipo de avion').
+
+respuestaHoraSalida():-
+    nl,
+    write('Indique la hora de salida deseada').
+
+respuestaHoraLlegada():-
+    nl,
+    write('Indique la hora de llegada deseada').
+
+respuestaDireccionSalida():-
+    nl,
+    write('Indique la direccion de salida porfavor').
+
+respuestaDireccionLlegada():-
+    nl,
+    write('Indique la direccion de llegada porfavor').
+
+respuestaSolicitudAterrizaje():-
+    nl,
+    write('Indique la velocidad, distancia a la pista y direccion de vuelo').
+
+respuestaEmergencia():-
+    nl,
+    write('Entendido! Indique la emergencia!').
+
+respuestaAgradecimiento():-
+    nl,
+    write('Con gusto!').
